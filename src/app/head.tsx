41e270ded12ebc3e6011a11395c6044443d4ffc0
Kumar Aryan <kuaryan1946@gmail.com>
2026-06-15 00:20:40 +0530

    feat: add explicit head.tsx for GSC verification

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="gY5g1qm8z9kqntYSZsIuSiEkptjhW4km1ssxBqDTQ10"
+      />
+    </>
+  );
+}
